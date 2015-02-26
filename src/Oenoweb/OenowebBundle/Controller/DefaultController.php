4eee0ab5acf7c44d94f3c22d85766ec002920281
EugeneDeRastignac <eugene.1835@gmail.com>
2015-02-26 18:33:30 +0100

    Création de OenowebBundle

---
+++ src/Oenoweb/OenowebBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Oenoweb\OenowebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('OenowebBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Oenoweb/OenowebBundle/OenowebBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oenoweb\OenowebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OenowebBundle extends Bundle
+{
+}
